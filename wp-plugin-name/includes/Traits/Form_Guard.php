f24eb3e658fe7b8a7360e1fb15803b251f3eb08f
Thomas Lellesch <60674859+lellesch@users.noreply.github.com>
2024-10-20 06:02:26 +0200

    Update Form_Guard.php

--- wp-plugin-name/includes/Traits/Form_Guard.php
+++ wp-plugin-name/includes/Traits/Form_Guard.php
@@ -8,31 +8,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 trait Form_Guard {
 
-
 	/**
-	 * Validates the form action by comparing the expected action name with the received action name.
+	 * Validates the security check by verifying the presence of required POST parameters,
+	 * user capabilities, and nonce consistency.
 	 *
-	 * @return bool True if the received action matches the expected action, false otherwise.
+	 * @return bool Returns true if the security check is valid, otherwise false.
 	 */
-	private function is_form_action_valid(): bool {
-		if ( ! isset( $_POST['action'] ) ) {
+	private function is_security_check_valid(): bool {
+
+		if ( ! isset( $_POST['action'] ) || ! isset( $_POST['_wpnonce'] ) || ! current_user_can( 'manage_options' ) ) {
 			return false;
 		}
 
 		$required_action = sanitize_text_field( $this->form_action_name );
 		$received_action = sanitize_text_field( wp_unslash( $_POST['action'] ) );
 
-		return $received_action === $required_action;
-	}
-
-	/**
-	 * Validates the security check by ensuring the nonce field is set and the user has the required capabilities.
-	 *
-	 * @return bool True if the nonce is verified and the user can manage options, false otherwise.
-	 */
-	private function is_security_check_valid(): bool {
-
-		if ( ! isset( $_POST['_wpnonce'] ) || ! current_user_can( 'manage_options' ) ) {
+		if ( $received_action !== $required_action ) {
 			return false;
 		}
 
